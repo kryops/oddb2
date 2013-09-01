aff303176e171b8a6e91ff392522bbb56c860c1d
Michael Strobel <michael@kryops.de>
2013-09-01 15:43:37 +0200

    Bugfix Passwort-Generierung beim Anlegen einer DB

--- admin/pages/dbs/add.php
+++ admin/pages/dbs/add.php
@@ -382,7 +382,7 @@ else if($_GET['sp'] == 'add_send') {
 						user_playerName = '".escape($data['playerName'])."',
 						user_allianzenID = ".$data['player_allianzenID'].",
 						userRechtelevel = 4,
-						userPassword = '".General::encryptPassword($pw, $config['key'])."',
+						userPassword = '".General::encryptPassword($pw, $config['instancekey'])."',
 						userSettings = '".escape($settings)."',
 						userApiKey = '".General::generateApiKey()."'
 				");
